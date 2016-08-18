cbed1a22b16ad61866110b399ba11817232e08ab
TIRTH RAJEN MEHTA <tirthmehta2294@gmail.com>
2016-08-17 19:38:43 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,505 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>MTC Home</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="styler.css">
+    <meta name="theme-color" content="#0073e6">
+    <link rel="stylesheet" type="text/css" href="animate.css">
+    <script src="wow.min.js"></script>
+    <script>
+      new WOW().init();
+    </script>
+</head>
+<style type="text/css">
+	body{padding-top: 40px}
+  .error{color:red;}
+</style>
+<body data-spy="scroll" data-target="#my-navbar">
+<!--Navbar starts-->
+<nav class="navbar navbar-inverse navbar-fixed-top" id="my-navbar">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle wow bounceInDown" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php">MTC</a>
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+      
+        <li ><a href="#feedback">Feedback</a></li>
+        <li ><a href="#price-list">Price Details</a></li>
+        
+        
+        <li ><a href="#faq">Faq</a></li>
+        <li ><a href="#about">About Us</a></li>
+        <li ><a href="#contact">Contact Us</a></li>
+        
+        </ul>
+        </div>
+        </div>
+        </nav>
+        <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+        <!--Navbar ends-->
+  
+<!--Jumbotron starts-->
+  <div class="jumbotron" style="background-color:#0073e6">
+  <div class="container text-center">
+    <h1 >MEHTA TRADING COMPANY </h1><span><img src="truck.png" class="wow shake" style="height:30px ;width:30px" /></span><h6><strong>Automobile Spare Parts Distribution</strong></h6></span>
+    <p>We offer the best products at the lowest rates</p>
+
+    <!--button-group starts-->
+<div class="btn-group btn-lg" role="group" aria-label="">
+  <a href="proj1-page2.php"><button type="button" class="btn btn-info" style="padding-left:30px; padding-right:30px ;background-color:#66b3ff">Place Order</button></a>
+  <a href="proj1-page3.php"><button type="button" class="btn btn-success" style="padding-left:20px; padding-right:20px; background-color:#70db70">Exquisite Products</button></a>
+</div>
+
+<!--button-group ends-->
+  </div>
+</div><!--Jumbotron ends-->
+
+<div class="container-fluid" id="feedback" style="min-height:15px"></div>
+<!--FEEDBACK-->
+<div class="container">
+	<section>
+		<div class="page-header">
+			<h2>Feedback <span class="glyphicon glyphicon-pencil"></span><br><small>(Obtained from the great)</small></h2>
+		</div>
+			<div class="row" >
+			<div class="col-md-4" style="background-color:#66b3ff">
+				<blockquote>
+					<p>Fastest Delivery system!</p>
+					<footer style="color:black">Rajkamal Auto Stores, Kolhapur</footer>
+				</blockquote>
+			</div>
+			<div class="col-md-4" style="background-color: #ff4d4d">
+				<blockquote>
+					<p>Great Packaging!</p>
+					<footer style="color:black">Vijay Automobiles, Solapur</footer>
+				</blockquote>
+			</div>
+			<div class="col-md-4" style="background-color:#70db70">
+				<blockquote>
+					<p>Great Offers!</p>
+					<footer style="color:black">Anand Auto Stores, Sangli</footer>
+				</blockquote>
+			</div>
+				
+			</div>
+	</section>
+</div>
+<!--FEEDBACK ENDS-->
+
+
+<!--CALL TO ACTION-->
+
+
+
+<!--Gallery-->
+
+
+
+
+
+
+
+
+
+<div class="container-fluid" id="price-list" style="min-height:35px"></div>
+
+
+<div class="container">
+	<section>
+	<div class="page-header">
+		<h2 class="wow fadeInDownBig">Price Details <span class="glyphicon glyphicon-list-alt"></span><br>
+		<small>(Have a look at our product details)</small>
+    
+		</h2>
+	</div>
+  <div>
+    <h3>SRMT PRICE LIST</h3>
+  </div>
+		<div class="row" style="padding-top:20px"> 
+			<div class="col-md-2 col-sm-2 wow flipInY" data-wow-delay="0.5s" data-wow-duration="0.5s">
+				<a href="Srmt.pdf" target="_blank"><button type="button" class="btn btn-info">SRMT Price List <span class="glyphicon glyphicon-download"></span></button></a>
+				
+			</div>
+      </div>
+      <hr>
+      <div><h3>SBP PRICE LIST</h3></div>
+      <div class="row" style="padding-top:20px"> 
+      <div class="col-md-4 col-sm-4 wow flipInY" data-wow-delay="0.5s" data-wow-duration="0.5s">
+        <a href="bdrum.pdf" target="_blank"><button type="button" class="btn btn-info">SBP Price List (Brake Drums) <span class="glyphicon glyphicon-download"></span></button></a>
+        
+      </div>
+      <div class="col-md-4 col-sm-4 wow flipInY" data-wow-delay="0.5s" data-wow-duration="0.5s">
+        <a href="Clutch.pdf" target="_blank"><button type="button" class="btn btn-info">SBP Price List (Clutch) <span class="glyphicon glyphicon-download"></span></button></a>
+        
+      </div>
+      <div class="col-md-4 col-sm-4 wow flipInY" data-wow-delay="0.5s" data-wow-duration="0.5s">
+        <a href="sh&b.pdf" target="_blank"><button type="button" class="btn btn-info">SBP Price List (Shackes & Brackets) <span class="glyphicon glyphicon-download"></span></button></a>
+        
+      </div>
+      </div>
+      <hr>
+			<div><h3>SBP CATALOGUES LIST</h3></div>
+      <div class="row" style="padding-top:20px"> 
+      <div class="col-md-4 col-sm-4 wow flipInY" data-wow-delay="0.5s" data-wow-duration="0.5s">
+        <a href="Sbp-1.pdf" target="_blank"><button type="button" class="btn btn-info">SBP Catalogue Brake Drums <span class="glyphicon glyphicon-download"></span></button></a>
+        
+      </div>
+      <div class="col-md-4 col-sm-4 wow flipInY" data-wow-delay="0.5s" data-wow-duration="0.5s">
+        <a href="Sbp-2.pdf" target="_blank"><button type="button" class="btn btn-info">SBP Catalogue Clutch Components <span class="glyphicon glyphicon-download"></span></button></a>
+        
+      </div>
+      <div class="col-md-4 col-sm-4 wow flipInY" data-wow-delay="0.5s" data-wow-duration="0.5s">
+        <a href="Sbp-3.pdf" target="_blank"><button type="button" class="btn btn-info">SBP Catalogue Shackles & Brackets <span class="glyphicon glyphicon-download"></span></button></a>
+        
+      </div>
+      </div>
+	<hr>		
+	</section>
+</div>
+
+<!--PRICE LIST ENDS-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="container" style="min-height:20px" id="faq"></div>
+
+<!--FAQ STARTS-->
+
+
+<div class="container">
+      <section>
+        <div class="page-header" >
+          <h2  class="wow fadeInDownBig">FAQs ?<br><small> (Engaging with consumers)</small></h2>
+        </div><!-- End Page Header -->
+
+        <div class="panel-group" id="accordion">
+          <div class="panel panel-default wow flipInY">
+            <div class="panel-heading">
+              <div class="panel-title">
+                <h4 data-toggle="collapse" data-parent="#accordion" style="text-decoration:none">
+                  <strong>Where can I find your products ?</strong>
+                </h4>
+              </div><!-- End panel title -->
+              <div id="collapse-1" class="panel-collapse collapse in">
+                <div class="panel-body">
+                  Our products can be found in the <a href="#price-list" style="color:blue; text-decoration:none">Price List</a> section. Please copy the code while placing an order.
+                </div>
+              </div><!-- End Panel collapse -->
+            </div>
+          </div>
+
+          <div class="panel panel-default wow flipInY">
+            <div class="panel-heading">
+              <div class="panel-title">
+                <h4 data-toggle="collapse" data-parent="#accordion">
+                  <strong>How do I place an order through the website ?</strong>
+                </h4>
+              </div><!-- End panel title -->
+              <div id="collapse-2" class="panel-collapse collapse in">
+                <div class="panel-body">
+                  You can go the <a href="proj1-page2.php" style="color:blue; text-decoration:none">Place Order</a> section in the website placed right at the top. Then simply fill out the form and in the message section simply mention the exact code and quantity of products you need. Once you press submit, we will receive your order. We will give you a confirmation call for the same. Click <a href="proj1-page2.php" style="color:blue; text-decoration:none">here</a> to go to our Place Order page.
+                </div>
+              </div><!-- End Panel collapse -->
+            </div>
+          </div>
+
+          <div class="panel panel-default wow flipInY">
+            <div class="panel-heading">
+              <div class="panel-title">
+                <h4 data-toggle="collapse" data-parent="#accordion">
+                  <strong>When will my order be dispatched?</strong>
+                </h4>
+              </div><!-- End panel title -->
+              <div id="collapse-3" class="panel-collapse collapse in">
+                <div class="panel-body">
+                  We dispatch all our products within 24hrs.
+                </div>
+              </div><!-- End Panel collapse -->
+            </div>
+          </div>
+          <div class="panel panel-default wow flipInY">
+            <div class="panel-heading">
+              <div class="panel-title">
+                <h4 data-toggle="collapse" data-parent="#accordion">
+                  <strong>What should I do if I am facing a technical glitch or I am unable to place my order?</strong>
+                </h4>
+              </div><!-- End panel title -->
+              <div id="collapse-2" class="panel-collapse collapse in">
+                <div class="panel-body">
+                  Do not worry, you can call us at anytime from our phone numbers mentioned in the <a href="#contact" style="color:blue; text-decoration:none">Contact Us</a> section.
+                </div>
+              </div><!-- End Panel collapse -->
+            </div>
+          </div>
+        </div><!-- End panel group -->
+
+      </section>
+    </div><!-- End container -->
+
+
+
+<!--FAQ ENDS-->
+<div class="container" id="about" style="min-height:20px"></div>
+
+
+
+<!--About US-->
+<div class="container">
+  <section>
+    <div class="page-header">
+      <h2 class="wow fadeInDownBig">About Us <span class="glyphicon glyphicon-user"></span></h2>
+    </div>
+
+  <div class="row">
+  <div class="col-md-4 col-sm-4">
+    <blockquote>
+    Mehta Trading Company has been active for more than 60 years in the field of automobile sparepart distribution. We have been dealing with a large number of customers. To provide optimum service and quality products is our motto. 
+      <footer>
+        Rajen Mehta (Partner: Mehta Trading Company)
+      </footer>
+          </blockquote>
+  </div>
+  <div class="col-md-8 col-sm-8">
+  <div class="container" style="max-width:700px; max-height:400px">
+<section>
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="5"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="6"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="rajen.jpg" style="height:400px; width:700px; " alt="Partner: Mehta Trading Company">
+      <div class="carousel-caption">
+        Partner: Mehta Trading Company
+      </div>
+    </div>
+    <div class="item">
+      <img src="gandhi.jpg"  style="height:400px; width:700px; " alt="Manager: Mehta Trading Company">
+      <div class="carousel-caption">
+        Manager: Mehta Trading Company
+      </div>
+    </div>
+    <div class="item">
+      <img src="staff-up.jpg"  style="height:400px; width:700px; " alt="Staff: Head Office">
+      <div class="carousel-caption">
+        Staff: Head Office
+      </div>
+    </div>
+    <div class="item">
+      <img src="staff-down.jpg"  style="height:400px; width:700px; " alt="Staff: Head Office">
+      <div class="carousel-caption">
+        Staff: Head Office
+      </div>
+    </div>
+    <div class="item">
+      <img src="kantibhai.jpg"  style="height:400px; width:700px; " alt="Staff: Head Office">
+      <div class="carousel-caption">
+        Staff: Head Office
+      </div>
+    </div>
+    <div class="item">
+      <img src="staff2.jpg"  style="height:400px; width:700px; " alt="Staff: Bhiwandi Godown">
+      <div class="carousel-caption">
+        Staff: Bhiwandi Godown
+      </div>
+    </div>
+    <div class="item">
+      <img src="staff3.jpg"  style="height:400px; width:700px; " alt="Staff: Bhiwandi Godown">
+      <div class="carousel-caption">
+        Staff: Bhiwandi Godown
+      </div>
+    </div>
+
+  </div>
+  </div>
+  </section>
+  </div>
+
+
+
+
+
+  </div>
+  </div>  
+    </section>
+    </div>
+    
+        
+  
+<!--CONTACT US-->
+
+<!-- Contact -->
+<div class="container-fluid" id="contact" style="min-height:15px"></div>
+  <div class="container">
+    <section>
+      <div class="page-header" >
+          <h2 class="wow fadeInDownBig">Contact Us <span class="glyphicon glyphicon-earphone"></span><br><small> (Contact us for more information)</small></h2>
+        </div><!-- End Page Header -->
+
+        <div class="row">
+          <div class="col-sm-3">
+            
+
+		
+            <address>
+              <strong>Mehta Trading Company <span class="glyphicon glyphicon-briefcase"></span></strong><br>
+             	22, Dhun Mansion,<br>
+             	20, Avantikabai Gokhale Marg,<br>
+             	 Opera House,<br>
+             	  Mumbai, Maharashtra 400004 <br>
+              	Contact No. 022 2382 4221<br>
+                Contact No. 022 2388 9960<br>
+                Mobile No. +91-9324668285
+            </address>
+            
+          </div>
+          <div class="col-sm-1">
+            
+          </div>
+          <div class="col-sm-4">
+            <address><strong>Timings <span class="glyphicon glyphicon-time"></span></strong><br>
+              <table class="table table-condensed" style="padding:0px">
+    
+    <tbody>
+      <tr>
+        <td>Monday</td>
+        <td>10 AM - 6.30 PM</td>
+      </tr>
+      <tr>
+        <td>Tuesday</td>
+        <td>10 AM - 6.30 PM</td>
+      </tr>
+      <tr>
+        <td>Wednesday</td>
+        <td>10 AM - 6.30 PM</td>
+      </tr>
+      <tr>
+        <td>Thursday</td>
+        <td>10 AM - 6.30 PM</td>
+      </tr>
+      <tr>
+        <td>Friday</td>
+        <td>10 AM - 6.30 PM</td>
+      </tr>
+      <tr>
+        <td>Saturday</td>
+        <td>10 AM - 4.30 PM</td>
+      </tr>
+      <tr>
+        <td>Sunday</td>
+        <td>Closed</td>
+      </tr>
+    </tbody>
+  </table>
+              
+            </address>
+          </div>
+          <div class="col-sm-4 maps">
+          <address><strong>Office-Location <span class="glyphicon glyphicon-map-marker"></span></strong></address>
+          	<iframe style="padding-top:-20px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d235.83981999817965!2d72.81693011941961!3d18.956293124220892!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7ce113fffffff%3A0x9ff04af936d3a0eb!2sMehta+Trading+Company!5e0!3m2!1sen!2sin!4v1467308253405" width="270" height="150" frameborder="0" style="border:0" allowfullscreen></iframe>
+          </div>
+        </div>
+       </section>
+       </div>
+
+       <div class="container-fluid" style="min-height:20px"></div>
+
+          
+<!--CONTACT US ENDS-->
+
+<!-- Footer -->
+
+<footer>
+<div class="container-fluid" style="background-color:#808080">
+<div class="row">
+<div class="col-md-4">
+  
+</div>
+<div class="col-md-4 text-center">
+  <h5 style="color:white">Copyright &copy; 2016 Mehta Trading Company | All rights reserved</h5>
+</div>
+<div class="col-md-2">
+</div>
+<div class="col-md-2 col-md-offset-2 text-center">
+    <h6 style="padding-left:55px ;padding-top:-20px">By: Tirth Rajen Mehta</h6>
+</div>
+        
+ 
+   </div>
+   </div>
+</footer>
+
+
+
+
+
+
+
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+<script src="jquery.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+  $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 2000);
+       return false;
+      }
+    }
+  });
+});
+</script>
+<script >
+  $(document).ready(function () {
+  $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
+});
+</script>
+
+
+</body>
+</html>
